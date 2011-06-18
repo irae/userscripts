23a5a5e029245bd6656f763618f11885a8e9aa8d
Iraê <irae@irae.pro.br>
2011-06-18 18:49:58 -0300

    Support for utm_page.

--- remove_utm_source_url.user.js
+++ remove_utm_source_url.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name           Remove utm_source from URLs
+// @name           Remove utm_source, utm_pages, etc. from URLs
 // @namespace      pro.irae.userscripts
-// @description    Remove seamlessly the long URL from feedburner, making it easy to share and to read tose urls
+// @description    Remove seamlessly the long URL from analytics, feedburner, etc. making it easy to share and to read tose urls
 // @include        *utm_source=*
+// @include        *utm_page=*
 // ==/UserScript==
 
 (function() {
-	var cleanURL = (document.location.href+'').replace(/(\?|&)?utm_source.*/,'');
+	var cleanURL = (document.location.href+'').replace(/(\?|&)?utm_.*/,'');
 	if(history && 'replaceState' in history) {
 		history.replaceState({},'same',cleanURL);
 	} else {
